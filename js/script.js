b8a611091b23cf2f7b99e4fbe673b2cfb1597c13
Aiyarat Mueankrut <Aiyarat.mkrut@gmail.com>
2026-03-10 03:05:57 +0700

    index finish

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-function scrollToSection(){
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show")
+        }
+    })
+})
 
-  const section = document.getElementById("content")
-
-  section.scrollIntoView({behavior:"smooth"})
-
-  setTimeout(()=>{
-    section.classList.remove("translate-y-20","opacity-0")
-  },300)
-
-}
+document.querySelectorAll(".fade-up").forEach(el => {
+    observer.observe(el)
+})
