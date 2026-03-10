172f8bdb3ab24c07da9122f5fd62144a246a5fbf
Aiyarat Mueankrut <Aiyarat.mkrut@gmail.com>
2026-03-10 12:47:37 +0700

    update ui

--- js/script.js
+++ js/script.js
@@ -9,3 +9,7 @@ const observer = new IntersectionObserver(entries => {
 document.querySelectorAll(".fade-up").forEach(el => {
     observer.observe(el)
 })
+
+function goTolistPage(comp){
+    window.location.href = "/page/List.html?comp=" + comp;
+}
